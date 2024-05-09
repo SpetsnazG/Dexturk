b670474f1836a9c3bee575f8f8b9024ee95e4299
SpetsnazG <51333837+SpetsnazG@users.noreply.github.com>
2024-05-09 20:06:14 +0300

    Update NetworkSwitcher.tsx
    base, basesepolia add

--- apps/aptos/components/NetworkSwitcher.tsx
+++ apps/aptos/components/NetworkSwitcher.tsx
@@ -11,7 +11,8 @@ import { aptosLogoClass } from './Logo/CurrencyLogo.css'
 const evmChains = [
   { id: 56, name: 'BNB Smart Chain' },
   { id: 1, name: 'Ethereum' },
-  { id: 8453, name: 'Base' },
+  { id: 84531, name: 'Base' },
+  { id: 84532, name: 'Base Sepolia' },
 ]
 
 const NetworkSelect = () => {
