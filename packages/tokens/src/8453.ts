565ffd1688579232f2926fe0ac1ee8230b191f64
SpetsnazG <51333837+SpetsnazG@users.noreply.github.com>
2024-05-10 07:31:47 +0300

    Delete packages/tokens/src/8453.ts

--- packages/tokens/src/8453.ts
+++
@@ -1,7 +0,0 @@
-import { ChainId, ERC20Token, WETH9 } from '@pancakeswap/sdk'
-import { USDC_BASE } from './common'
-
-export const baseTokens = {
-    weth: WETH9[ChainId.BASE],
-    usdc: USDC_BASE,
-}
